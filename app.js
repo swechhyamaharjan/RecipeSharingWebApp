28856119d4c01e3e1a6e7fd74deb147586ee8d5b
Swechhya Maharjan <swekchya.mjn@gmail.com>
2025-09-01 12:14:56 +0545

    Login and Signup created

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from "express"
+import userRouter from "./router/userRouter.js"
 
 const app = express();
+app.use(express.json());
+app.use("/api/users", userRouter);
 
 export default app;
---
+++ controller/userController.js
@@ -0,0 +1,35 @@
+import User from "../model/user.js"
+
+const signup = async (req, res) => {
+  const { fullname, email, password, isAdmin } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).send({ error: "User already exits" });
+  }
+  const newUser = await User.create({
+    fullname,
+    email,
+    password,
+    isAdmin
+  })
+  res.send({ message: "User created successfully", User: newUser });
+}
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).send({ error: "User doesn't exits of this email!" })
+  if (user.password === password) {
+    res.send({
+      message: "Login success", user: {
+        fullname: user.fullname,
+        email: user.email,
+        isAdmin: user.isAdmin
+      }
+    })
+  }else{
+    res.status(400).send({error: "Password doesn't match"})
+  }
+}
+export { signup, login };
+
