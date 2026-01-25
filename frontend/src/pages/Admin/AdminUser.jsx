cc030e5bcae8c9e2610d424b6b9616e403e2a045
Swechhya Maharjan <swekchya.mjn@gmail.com>
2026-01-25 17:27:50 +0545

    Added User Management in Admin Side

--- frontend/src/pages/Admin/AdminUser.jsx
+++ frontend/src/pages/Admin/AdminUser.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { useGetAllUsersQuery } from '../../slices/userapiSlice'
+import Loader from "../../components/Loader.jsx";
+import Message from "../../components/Message.jsx";
 
 const AdminUser = () => {
+  const {data: users = [], isLoading, error} = useGetAllUsersQuery();
+
+  if (isLoading) return <Loader />
+  if (error) return <Message>{error.message || "Failed to load users"}</Message>
+
   return (
-    <div>AdminUser</div>
+    <div className='min-h-screen p-6 bg-gray-50'>
+      <h1 className='text-2xl font-bold mb-6 text-gray-700'>All Users</h1>
+
+      <div>
+        <table className='overflow-x-auto bg-white shadow rounded-lg border border-gray-200'>
+          <thead className='bg-gray-100'>
+            <tr>
+              <th className='px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider'>ID</th>
+              <th className='px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider'>Name</th>
+              <th className='px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider'>Email</th>
+              <th className='px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider'>Joined On</th>
+            </tr>
+          </thead>
+
+          <tbody className='divide-y divide-gray-200'>
+            {users.map((user, index)=>(
+              <tr key={user._id}>
+                <td className='px-6 whitespace-nowrap text-gray-700 text-sm'>{index+1}</td>
+                <td className='px-6 whitespace-nowrap text-gray-700 text-sm'>{user.fullname}</td>
+                <td className='px-6 whitespace-nowrap text-gray-700 text-sm'>{user.email}</td>
+                <td className='px-6 whitespace-nowrap text-gray-700 text-sm'>{new Date(user.createdAt).toLocaleDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
