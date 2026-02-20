d04879a262f3c53c3f935e7c733f12321551c80c
Swechhya Maharjan <swekchya.mjn@gmail.com>
2026-02-20 10:08:12 +0545

    enable cross-site cookies

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken"
 
-const createToken = (res, _id, rememberMe = false)=>{
-   const expiresIn = rememberMe ? "30d" : "1d";
-  const token = jwt.sign({_id}, process.env.JWT_SECRET, {expiresIn});
+const createToken = (res, _id, rememberMe = false) => {
+  const expiresIn = rememberMe ? "30d" : "1d";
+  const token = jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn });
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: false,          
-    sameSite: "lax",
+    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    sameSite: process.env.NODE_ENV !== "development" ? "None" : "Lax", // "None" for cross-site
     maxAge: rememberMe
       ? 30 * 24 * 60 * 60 * 1000
       : 24 * 60 * 60 * 1000,
