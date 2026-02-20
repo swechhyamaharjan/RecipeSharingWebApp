ef430c7275c0705719c1431fa7d353aa1e52a71e
Swechhya Maharjan <swekchya.mjn@gmail.com>
2026-02-20 09:43:25 +0545

    vercel setup

---
+++ backend/api/index.js
@@ -0,0 +1,6 @@
+import app from "../app.js";
+import connectDB from "../utils/db.js";
+
+connectDB();
+
+export default app;
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 export const RECIPE_URL = `/api/recipes`;
 export const USER_URL = "/api/users"
 export const CATEGORY_URL = "/api/category"
